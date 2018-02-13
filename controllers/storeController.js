345960f1e954138225b9b169bb168906a9720abe
Viral Solani <viral.solani@gmail.com>
2018-02-13 20:49:26 +0530

    Using Async Await while creating store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,36 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+/**
+ * Index Page
+ *
+ * @param req
+ * @param res
+ * @return {*}
+ */
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
+/**
+ * add store Page
+ *
+ * @param req
+ * @param res
+ * @return {*}
+ */
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+/**
+ * creating stroe
+ *
+ * @param req
+ * @param res
+ * @return {*}
+ */
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
 };
