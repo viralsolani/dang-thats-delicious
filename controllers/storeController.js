0279d0ceed87914cba0e6d834cab031da9d880b0
Viral Solani <viral.solani@gmail.com>
2018-02-24 20:42:34 +0530

    Uploading and Resizing Images with Middleware

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,75 +4,65 @@ const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
 
-/**
- * Index Page
- *
- * @param req
- * @param res
- * @return {*}
- */
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
+    }
+  }
+};
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
-/**
- * add store Page
- *
- * @param req
- * @param res
- * @return {*}
- */
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: 'Add Store'});
+  res.render('editStore', { title: 'Add Store' });
 };
 
-/**
- * Adding Store
- *
- * @param req
- * @param res
- * @return {*}
- */
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going!
+  next();
+};
+
 exports.createStore = async (req, res) => {
-    const store = await (new Store(req.body)).save();
-    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-    res.redirect(`/store/${store.slug}`);
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
 
-
-/**
- * Getting all store
- *
- * @param req
- * @param res
- * @return {*}
- */
 exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 };
 
-/**
- * Edit Store
- *
- * @param req
- * @param res
- * @return {*}
- */
 exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
   const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
   // TODO
-  //confirm they are the owner of the store
+  // 3. Render out the edit form so the user can update their store
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
 
-/**
- * Updating Store
- *
- * @param req
- * @param res
- * @return {*}
- */
 exports.updateStore = async (req, res) => {
   // set the location data to be a point
   req.body.location.type = 'Point';
