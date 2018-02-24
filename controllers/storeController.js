fffc8c887d8d6551be3d1b53ba94d960ff2198fb
Viral Solani <viral.solani@gmail.com>
2018-02-24 19:48:42 +0530

    wip

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
 /**
  * Index Page
  *
