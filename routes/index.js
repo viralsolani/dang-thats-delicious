696a495716e5f5d169260bda67b359a32f304274
Viral Solani <viral.solani@gmail.com>
2018-02-25 00:28:56 +0530

    Routing and Templating Single Stores

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,6 @@ router.post('/add', catchErrors(storeController.createStore));
 router.post('/add/:id', catchErrors(storeController.updateStore));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
 module.exports = router;
