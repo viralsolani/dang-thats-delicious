4eed1dbe113ddaf1dd4ac354bc371372975cd2e0
Viral Solani <viral.solani@gmail.com>
2018-02-25 20:13:21 +0530

    - Creating User Accounts - Login And Registration Form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController  = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 
@@ -28,6 +29,10 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
 
 
 module.exports = router;
