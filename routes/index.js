2f51ef429b50fa3d32c60ad8d7818003fb40cd07
Viral Solani <viral.solani@gmail.com>
2018-02-26 12:06:50 +0530

    - Creating a User Account Edit Screen - Update Account

--- routes/index.js
+++ routes/index.js
@@ -50,4 +50,7 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
