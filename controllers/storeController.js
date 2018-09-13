725f1cab9455e7db55a0ec599403fd88097efdfd
Viral Solani <viral.solani@gmail.com>
2018-09-13 14:37:32 +0530

    resolve bug

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const User = mongoose.model('User');
 const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
