6dc707f89231d5e2c85cca17ca54395cd6e4db3e
Viral Solani <viral.solani@gmail.com>
2018-09-13 22:30:36 +0530

    Feature : Adding a Review for Store

--- start.js
+++ start.js
@@ -8,7 +8,9 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 }
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({
+  path: 'variables.env'
+});
 
 // Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE);
@@ -22,6 +24,7 @@ mongoose.connection.on('error', (err) => {
 // import all of our models
 require('./models/Store');
 require('./models/User');
+require('./models/Review');
 
 
 // Start our app!
